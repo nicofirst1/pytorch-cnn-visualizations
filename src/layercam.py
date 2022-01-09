d0cdaa130252c5ce617ed4669fad3679dcb217c3
dizzi <nicofirst1@gmail.com>
2022-01-09 11:23:46 +0100

    fixed for gray image

--- src/layercam.py
+++ src/layercam.py
@@ -3,23 +3,22 @@ Created on Mon Jul 5 12:39:11 2021
 
 @author: Peng-Tao Jiang - github.com/PengtaoJiang
 """
-from PIL import Image
 import numpy as np
 import torch
-
-
+from PIL import Image
 
 
 class LayerCam():
     """
         Produces class activation map
     """
+
     def __init__(self, model, extractor):
         self.model = model
         self.model.eval()
         # Define extractor
         self.extractor = extractor
-        self.name="LayerCam"
+        self.name = "LayerCam"
 
     def generate_cam(self, input_image, target_class=None):
         # Full forward pass
@@ -42,13 +41,18 @@ class LayerCam():
         target = conv_output.cpu().data.numpy()
         # Get weights from gradients
         weights = guided_gradients
-        weights[weights < 0] = 0 # discard negative gradients
+        weights[weights < 0] = 0  # discard negative gradients
         # Element-wise multiply the weight with its conv output and then, sum
         cam = np.sum(weights * target, axis=0)
         cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))  # Normalize between 0-1
         cam = np.uint8(cam * 255)  # Scale between 0-255 to visualize
+
+        # can't resize third dimension with 3 color channels, so bring to gray
+        if cam.shape[-1] % 3 != 0:
+            cam = cam.reshape((cam.shape[0], -1))
+
+        cam = cam.astype(np.uint8)
         cam = np.uint8(Image.fromarray(cam).resize((input_image.shape[2],
-                       input_image.shape[3]), Image.ANTIALIAS))/255
+                                                    input_image.shape[3]), Image.ANTIALIAS)) / 255
 
         return cam
-
